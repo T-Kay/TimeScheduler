a202e55dbf8e8bd9868015b2bf2b0e267b18ad62
T-Kay <ytlol@live.jp>
2016-09-03 21:51:18 +0900

    Set Layout Sign Up Form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
-  VALID_USERNAME_REGEX =  /^[0-9a-zA-Z_-]+$/
+  VALID_REGEX =  /\A[0-9a-zA-Z_-]+\z/i
   before_save { self.username = username.downcase }
   validates :username, presence: true, length: { minimum: 5,maximum: 50 },
-            format: { with: VALID_USERNAME_REGEX },
+            format: { with: VALID_REGEX },
             uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6,maximum: 50 },
+            format: { with: VALID_REGEX }
 end
